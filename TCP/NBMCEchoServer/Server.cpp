f562b0d812db19645c1cf5ae6504a98ded338812
hnoguchi <>
2024-01-24 14:25:24 +0900

    Server.cpp/hppのリファクタリング。

--- TCP/NBMCEchoServer/Server.cpp
+++ TCP/NBMCEchoServer/Server.cpp
@@ -1,7 +1,12 @@
 #include "./Server.hpp"
 #include "../utils/utils.hpp"
 
-static void	setFdFlags(const int &fd, int setFlags) {
+// static void fatalError(const std::string& message) {
+// 	std::perror(message.c_str());
+//     exit(EXIT_FAILURE);
+// }
+
+static void	setFdFlags(const int fd, const int setFlags) {
 	int	flags = 0;
 
 	if ((flags = fcntl(fd, F_GETFL, 0)) < 0) {
@@ -13,52 +18,55 @@ static void	setFdFlags(const int &fd, int setFlags) {
 	}
 }
 
-Server::Server(unsigned short port) :
-	socketFd_(0), socketAddressLen_(sizeof(this->socketAddress_)), maxClients_(5) {
-	// サーバーソケットの作成
-	this->socketFd_ = socket(AF_INET, SOCK_STREAM, 0);
-	if (this->socketFd_ < 0) {
+Server::Server(unsigned short port) : \
+	socketFd_(0), socketAddressLen_(sizeof(socketAddress_)), maxClients_(5) {
+		initializeServerSocket(port);
+		initializeClientSockets();
+}
+
+void	Server::initializeServerSocket(unsigned short port) {
+	socketFd_ = socket(AF_INET, SOCK_STREAM, 0);
+	if (socketFd_ < 0) {
 		fatalError("socket");
 	}
 
-	setFdFlags(this->socketFd_, O_NONBLOCK);
+	setFdFlags(socketFd_, O_NONBLOCK);
 
-	memset(&this->socketAddress_, 0, sizeof(this->socketAddress_));
-	this->socketAddress_.sin_family = AF_INET;
-	this->socketAddress_.sin_addr.s_addr = INADDR_ANY;
-	this->socketAddress_.sin_port = htons(port);
+	memset(&socketAddress_, 0, sizeof(socketAddress_));
+	socketAddress_.sin_family = AF_INET;
+	socketAddress_.sin_addr.s_addr = INADDR_ANY;
+	socketAddress_.sin_port = htons(port);
 
-	// サーバーソケットをアドレスにバインド
-	if (bind(this->socketFd_, reinterpret_cast<struct sockaddr *>(&this->socketAddress_), sizeof(this->socketAddress_)) < 0) {
+	if (bind(socketFd_, reinterpret_cast<struct sockaddr*>(&socketAddress_), \
+				sizeof(socketAddress_)) < 0) {
 		fatalError("bind");
 	}
-	// サーバーソケットをリスニング
-	if (listen(this->socketFd_, 3) < 0) {
+	if (listen(socketFd_, 3) < 0) {
 		fatalError("listen");
 	}
-	std::cout << "サーバーが " << port << " ポートでリスニングしています..." << std::endl;
+	std::cout << "Server is listening on port " << port << "..." << std::endl;
+}
 
-	// 各ソケットを初期化
-	this->fds_[0].fd = this->socketFd_;
-	for (int i = 1; i < this->maxClients_; ++i) {
-		this->fds_[i].fd = -1;
+void	Server::initializeClientSockets() {
+	fds_[0].fd = socketFd_;
+	for (int i = 1; i <= maxClients_; ++i) {
+		fds_[i].fd = -1;
 	}
-	// STDIN_FILENOは、ノンブロッキングにすべき？
-	this->fds_[this->maxClients_].fd = STDIN_FILENO;
-	for (int i = 0; i <= this->maxClients_; ++i) {
-		this->fds_[i].events = POLLIN;
-		this->fds_[i].revents = 0;
+	fds_[maxClients_].fd = STDIN_FILENO;
+	for (int i = 0; i <= maxClients_; ++i) {
+		fds_[i].events = POLLIN;
+		fds_[i].revents = 0;
 	}
 }
 
 Server::~Server() {
-	close(this->socketFd_);
+	close(socketFd_);
 }
 
 void	Server::run() {
 	while (1) {
-		// poll()を使用して待機
-		int	result = poll(this->fds_, this->maxClients_ + 1, 3 * 1000);
+		int result = poll(fds_, maxClients_ + 1, 3 * 1000);
+
 		if (result == -1) {
 			fatalError("poll");
 		}
@@ -66,93 +74,132 @@ void	Server::run() {
 			std::cout << "poll: Timeout 3 seconds..." << std::endl;
 			continue;
 		}
-		// サーバーソケットに新しい接続があるか確認
-		if (this->fds_[0].revents & POLLIN) {
-			// createNewSocket(this->socketFd_, &this->socketAddress_, &this->socketAddressLen_));
-			int	newSocket = -1;
-			newSocket = accept(this->socketFd_, reinterpret_cast<struct sockaddr *>(&this->socketAddress_), &this->socketAddressLen_);
-			if (newSocket < 0) {
-					close(newSocket);
-					fatalError("accept");
-			}
-			setFdFlags(newSocket, O_NONBLOCK);
-			// 新しいクライアントソケットを検出し、fdsに追加
-			for (int i = 1; i <= this->maxClients_; ++i) {
-				if (this->fds_[i].fd == -1) {
-					this->fds_[i].fd = newSocket;
-					std::cout << "新しいクライアントが接続しました。ソケット " << newSocket << std::endl;
-					break;
-				}
-			}
-		} else if (this->fds_[this->maxClients_].revents & POLLIN) {
-			std::string	input;
-			std::getline(std::cin, input);
-			if (input == "exit") {
-				std::cout << "See You..." << std::endl;
-				break;
-			}
-		}
-		// クライアントソケットにデータがあるか確認
-		for (int i = 1; i <= this->maxClients_; ++i) {
-			if (this->fds_[i].fd != -1 && (this->fds_[i].revents & POLLIN)) {
-				while (1) {
-					char		buffer[1024] = {0};
-					ssize_t		recvMsgSize = 0;
-
-					while (1) {
-						// sleep(2);
-						recvMsgSize = recv(this->fds_[i].fd, buffer, sizeof(buffer), MSG_DONTWAIT);
-						if (recvMsgSize < 0) {
-							if (errno == EAGAIN || errno == EWOULDBLOCK) {
-								// std::cout << "No data received." << std::endl;
-								errno = 0;
-								recvMsgSize = 0;
-								continue;
-							} else if (errno == ECONNRESET) {
-								recvMsgSize = 0;
-								break;
-							} else {
-								// std::cerr << errno << std::endl;
-								fatalError("recv");
-							}
-						} else {
-							break;
-						}
-					}
-					// クライアントが切断された場合
-					if (recvMsgSize == 0) {
-						std::cout << "クライアントソケット " << this->fds_[i].fd << " が切断されました。" << std::endl;
-						close(this->fds_[i].fd);
-						this->fds_[i].fd = -1;
-						break;
-					}
-					// クライアントからのデータを処理
-					std::cout << "クライアントソケット " << this->fds_[i].fd << " からのメッセージ: " << buffer << std::endl;
-					while (1) {
-						// クライアントに応答
-						ssize_t	sendMsgSize = 0;
-
-						sendMsgSize = send(this->fds_[i].fd, buffer, strlen(buffer), 0);
-						if (sendMsgSize < 0) {
-							if (errno == EAGAIN || errno == EWOULDBLOCK) {
-								std::cout << "No data sent." << std::endl;
-								errno = 0;
-								sendMsgSize = 0;
-								continue;
-							} else {
-								fatalError("send");
-							}
-						} else if (sendMsgSize != recvMsgSize) {
-							fatalError("send");
-						} else {
-							break;
-						}
-					}
-					break;
-				}
-			}
+		handleServerSocket();
+		handleStandardInput();
+		for (int i = 1; i <= maxClients_; ++i) {
+			handleClientSocket(i);
 		}
 	}
+}
+
+void	Server::handleServerSocket() {
+	if (!(fds_[0].revents & POLLIN)) {
+		return;
+	}
+	int newSocket = accept(socketFd_, \
+			reinterpret_cast<struct sockaddr*>(&socketAddress_), &socketAddressLen_);
+	if (newSocket < 0) {
+		close(newSocket);
+		fatalError("accept");
+	}
+	setFdFlags(newSocket, O_NONBLOCK);
+	for (int i = 1; i <= maxClients_; ++i) {
+		if (fds_[i].fd == -1) {
+			fds_[i].fd = newSocket;
+			std::cout << "New client connected. Socket: " \
+				<< newSocket << std::endl;
+			break;
+		}
+	}
+}
+
+void	Server::handleStandardInput() {
+	if (fds_[maxClients_].revents & POLLIN) {
+		std::string	input;
+		std::getline(std::cin, input);
+		if (input == "exit") {
+			std::cout << "See You..." << std::endl;
+			exit(0);
+		}
+	}
+}
+
+void	Server::handleClientSocket(int clientIndex) {
+	if (fds_[clientIndex].fd != -1 && (fds_[clientIndex].revents & POLLIN)) {
+		handleReceivedData(clientIndex);
+	}
+}
+
+void	Server::handleReceivedData(int clientIndex) {
+	char	buffer[1024] = {0};
+	ssize_t	sendMsgSize = 0;
+	ssize_t	recvMsgSize = 0;
+
+	recvMsgSize = recvNonBlocking(clientIndex, buffer, sizeof(buffer) - 1);
+	if (recvMsgSize == 0) {
+		handleClientDisconnect(clientIndex);
+	} else if (recvMsgSize < 0) {
+		fatalError("recv");
+	}
+	std::cout << "Client socket " << fds_[clientIndex].fd \
+		<< " message: " << buffer << std::endl;
+	sendMsgSize = sendNonBlocking(clientIndex, buffer, recvMsgSize);
+	if (sendMsgSize == 0) {
+		handleClientDisconnect(clientIndex);
+	} else if (sendMsgSize < 0) {
+		fatalError("send");
+	}
+	if (recvMsgSize != sendMsgSize) {
+		fatalError("send");
+	}
+}
+
+ssize_t Server::recvNonBlocking(int clientIndex, char* buffer, \
+		size_t bufferSize) {
+	ssize_t	recvMsgSize = 0;
+
+	while (1) {
+		recvMsgSize = recv(fds_[clientIndex].fd, buffer, \
+				bufferSize, MSG_DONTWAIT);
+
+		if (recvMsgSize >= 0) {
+			break;
+		}
+		if (errno == EAGAIN || errno == EWOULDBLOCK) {
+			errno = 0;
+			recvMsgSize = 0;
+			continue;
+		} else if (errno == ECONNRESET) {
+			recvMsgSize = 0;
+			break;
+		} else {
+			fatalError("recv");
+		}
+	}
+	return (recvMsgSize);
+}
+
+ssize_t	Server::sendNonBlocking(int clientIndex, const char* buffer, \
+		size_t dataSize) {
+	ssize_t sendMsgSize = 0;
+
+	while (1) {
+		sendMsgSize = send(fds_[clientIndex].fd, buffer, \
+				dataSize, MSG_DONTWAIT);
+
+		if (sendMsgSize >= 0) {
+			break;
+		}
+		if (errno == EAGAIN || errno == EWOULDBLOCK) {
+			std::cout << "No data sent." << std::endl;
+			errno = 0;
+			sendMsgSize = 0;
+			continue;
+		} else if (errno == ECONNRESET) {
+			sendMsgSize = 0;
+			break;
+		} else {
+			fatalError("send");
+		}
+	}
+	return (sendMsgSize);
+}
+
+void	Server::handleClientDisconnect(int clientIndex) {
+	std::cout << "Client socket " << fds_[clientIndex].fd \
+		<< " disconnected." << std::endl;
+	close(fds_[clientIndex].fd);
+	fds_[clientIndex].fd = -1;
 }
 
 int	main() {
